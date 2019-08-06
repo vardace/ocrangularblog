34fdc017219a155d0dbb0c0b50e51dd2be17fa86
Hudson <acharpentier.44@gmail.com>
2019-08-06 21:43:58 +0200

    sure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ocr-exercice-blog';
+  posts = [{
+    title: "J'apprends à programmer",
+    content: "Ceci est un test",
+    loveIts: 0,
+    created_at: new Date()
+  }, {
+    title: "Je persiste !",
+    content: "Même si c'est dur",
+    loveIts: 1,
+    created_at: new Date()
+  }, {
+    title: "Il le faut !",
+    content: "Je taf bientot dessus et ils pensent que je sais faire.",
+    loveIts: -1,
+    created_at: new Date()
+  }, {
+    title: "Liste de course",
+    content: "Lait en poudre. C'est tout. 150 paquets de lait en poudre",
+    loveIts: 3,
+    created_at: new Date()
+  }
+  ]
 }
